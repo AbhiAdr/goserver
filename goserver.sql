331022ed732b348d6da8470aa5b04018640b7af2
adrabhi <adrabhi@gmail.com>
2018-02-23 00:41:08 +0530

    sql dump file

--- goserver.sql
+++ goserver.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `project` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `project`;
---- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
----
---- Host: localhost    Database: project
---- ------------------------------------------------------
---- Server version  5.7.20-0ubuntu0.16.04.1
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: project
+-- ------------------------------------------------------
+-- Server version	5.7.21-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,9 +15,9 @@ USE `project`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
----
---- Table structure for table `users`
----
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -36,9 +34,9 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
----
---- Dumping data for table `users`
----
+--
+-- Dumping data for table `users`
+--
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
@@ -55,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---- Dump completed on 2017-11-29  4:13:51
+-- Dump completed on 2018-02-23  0:33:26
